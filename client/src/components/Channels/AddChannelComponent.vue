3b401b57322c98aad89124df10ac8b428b8cf386
DadyshkaGoga <95309197+DadyshkaGoga@users.noreply.github.com>
2025-06-16 17:26:40 +0300

    [scrap tg]

---
+++ client/src/components/Channels/AddChannelComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <el-input placeholder="url канала">
+
+    </el-input>
+
+    <el-button>
+      Добавить канал
+    </el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/Channels/AddChannel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Layout from "@/views/Layout.vue";
+</script>
+
+<template>
+
+  <Layout>
+    <h1>
+      Добавить канал
+    </h1>
+  </Layout>
+
+</template>
+
+<style scoped>
+
+</style>
